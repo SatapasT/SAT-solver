83a3d92d64f34558c4ea33ad0e06e1a2a35e41c1
Satapas T <satapastanachotpaisit@gmail.com>
2024-03-08 05:15:13 +0000

    optimized the progarm

--- skeletonCode_new.py
+++ skeletonCode_new.py
@@ -14,7 +14,6 @@ def load_dimacs(file_name):
 
 
 def simple_sat_solve(clause_set):
-    
     unique_literals = set()
     for clause in clause_set:
         for literal in clause:
@@ -46,33 +45,53 @@ def simple_sat_solve(clause_set):
             return answer
         
     return False
-    
 
 def branching_sat_solve(clause_set, partial_assignment):
     if isinstance(partial_assignment, list):
-        temp_assignment = {}
+        new_assignment = {}
         for literal in partial_assignment:
-            temp_assignment[abs(literal)] = literal > 0
-        partial_assignment = temp_assignment
-
-    if not clause_set:
-        return [i if partial_assignment[i] else -i for i in partial_assignment]
+            new_assignment[abs(literal)] = literal > 0
+        partial_assignment = new_assignment
+    
+    if len(clause_set) == 0 and len(partial_assignment) == 0 :
+        return []
+    elif len(clause_set) == 0 and len(partial_assignment) > 0:
+        return False
 
     if all_clauses_satisfy(clause_set, partial_assignment):
-        return [i if partial_assignment[i] else -i for i in partial_assignment]
+
+        unique_literals = set()
+        for clause in clause_set:
+            for literal in clause:
+                unique_literals.add(abs(literal))
+        
+        for literal in unique_literals:
+            if literal not in partial_assignment:
+                partial_assignment[literal] = True
+
+        result = []
+
+        for i in sorted(partial_assignment.keys()):
+            if partial_assignment.get(i, False):
+                result.append(i)
+            else:
+                result.append(-i)
+                
+        return result 
 
     variable = find_unassigned_variable(clause_set, partial_assignment)
-    if variable is None: 
+    if variable is None:
         return False
 
     for value in [True, False]:
-        new_partial_assignment = copy.deepcopy(partial_assignment)
-        new_partial_assignment[variable] = value
-        answer = branching_sat_solve(clause_set, new_partial_assignment)
+        partial_assignment[variable] =  value
+        answer = branching_sat_solve(clause_set, partial_assignment)
         if answer:
-            return answer  
+            return answer
+        del partial_assignment[variable]
 
     return False
+
 
 def all_clauses_satisfy(clause_set,partial_assignment):
     for clause in clause_set:
@@ -90,13 +109,13 @@ def all_clauses_satisfy(clause_set,partial_assignment):
         
     return True
 
+
 def find_unassigned_variable(clause_set, partial_assignment):
     for clause in clause_set:
         for literal in clause:
             if abs(literal) not in partial_assignment:
                 return abs(literal)
     return None
-
 
 
 def unit_propagate(clause_set):
@@ -125,10 +144,76 @@ def unit_propagate(clause_set):
     return clause_set
 
 
-def dpll_sat_solve(clause_set,partial_assignment):
-    ...
+def dpll_sat_solve(clause_set, partial_assignment, unique_literals=None):
 
+    if isinstance(partial_assignment, list):
+        new_assignment = {}
+        for literal in partial_assignment:
+            new_assignment[abs(literal)] = literal > 0
+        partial_assignment = new_assignment
+    
+    if unique_literals is None:
+        unique_literals = set()
+        for clause in clause_set:
+            for literal in clause:
+                unique_literals.add(abs(literal))
+    
+    if len(clause_set) == 0 and len(partial_assignment) == 0 :
+        return []
+    elif len(clause_set) == 0 and len(partial_assignment) > 0:
+        return False
 
+    unit_clauses = set()
+    for clause in clause_set:
+        if len(clause) == 1:
+            unit_clauses.add(tuple(clause))
+
+    while unit_clauses:
+        unit = list(unit_clauses.pop())
+        partial_assignment[abs(unit[0])] = unit[0] > 0
+        new_clause_set = []
+        for clause in clause_set:
+            if unit[0] in clause:
+                continue 
+            elif -unit[0] in clause:
+                new_clause = [x for x in clause if x != -unit[0]] 
+                if len(new_clause) == 1:
+                    unit_clauses.add(tuple(new_clause))
+                elif len(new_clause) > 0:
+                    new_clause_set.append(new_clause)
+            else:
+                new_clause_set.append(clause)
+
+        clause_set = new_clause_set
+
+    if all_clauses_satisfy(clause_set, partial_assignment):
+        
+        for literal in unique_literals:
+            if literal not in partial_assignment:
+                partial_assignment[literal] = True
+
+        result = []
+
+        for i in sorted(partial_assignment.keys()):
+            if partial_assignment.get(i, False):
+                result.append(i)
+            else:
+                result.append(-i)
+                
+        return result 
+
+    variable = find_unassigned_variable(clause_set, partial_assignment)
+    if variable is None:
+        return False
+
+    for value in [True, False]:
+        partial_assignment[variable] = value
+        answer = dpll_sat_solve(clause_set, partial_assignment,unique_literals)
+        if answer:
+            return answer
+        del partial_assignment[variable]
+
+    return False
 
 def test():
     print("Testing load_dimacs")
@@ -199,10 +284,33 @@ def test():
     print("Finished tests")
 
 test()
-sat1 = [[3, -1], [1, 1], [-4, 4], [2, -4, 4]]
-#sat1 = load_dimacs("8queens.txt")
-print(branching_sat_solve(sat1,[-2]))
+clause_set = [[1],[3, -1], [1, 1], [-4, 4], [2, -4, 4],[2],[5],[-5,2,3],[-6],[6,1,2],[1,2,3,4,5,6,7,8,9],[-8],[-1,-2,-3,-4,-5,-6,-7,-8,-9]]
+#sat1 = load_dimacs("LNP-6.txt")
+#print(dpll_sat_solve(clause_set,[]))
+
+# Measure runtime for simple_sat_solve
+start_time = time.time()
+for i in range(1000):
+    simple_sat_result = simple_sat_solve(clause_set)
+end_time = time.time()
+simple_sat_runtime = end_time - start_time
+
+# Measure runtime for branching_sat_solve with an empty initial partial assignment
+start_time = time.time()
+for i in range(1000):
+    dpll_sat_solve_result = dpll_sat_solve(clause_set, [])
+end_time = time.time()
+dpll_sat_solve_runtime = end_time - start_time
 
 
+# Measure runtime for branching_sat_solve with an empty initial partial assignment
+start_time = time.time()
+for i in range(1000):
+    branching_sat_result = branching_sat_solve(clause_set, [])
+end_time = time.time()
+branching_sat_runtime = end_time - start_time
 
 
+print("simple_sat_solve runtime: ", simple_sat_runtime, simple_sat_result)
+print("branching_sat_solve runtime: ", branching_sat_runtime,branching_sat_result )
+print("dpll_sat_solve runtime: ", dpll_sat_solve_runtime,dpll_sat_solve_result )
